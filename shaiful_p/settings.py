33be16e4498140a95715e9fc6723382b9cba7298
islam <md.shaiful.islam@protonmail.com>
2021-03-26 11:11:12 +0100

    BUG FIX

--- shaiful_p/settings.py
+++ shaiful_p/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
