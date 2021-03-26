92635e3f6c62c662fead230f045666ac64727a19
islam <md.shaiful.islam@protonmail.com>
2021-03-26 15:03:09 +0100

    index changes

--- shaiful_p/settings.py
+++ shaiful_p/settings.py
@@ -86,7 +86,12 @@ WSGI_APPLICATION = 'shaiful_p.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 import dj_database_url
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
