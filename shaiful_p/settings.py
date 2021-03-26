95ad401468a451dd37c85ea95290738f02982db0
islam <md.shaiful.islam@protonmail.com>
2021-03-26 12:02:41 +0100

    Database FIXING

--- shaiful_p/settings.py
+++ shaiful_p/settings.py
@@ -85,12 +85,10 @@ WSGI_APPLICATION = 'shaiful_p.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
+DATABASES = {}
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -139,7 +137,4 @@ STATICFILES_DIRS = (
 # For sending emails
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-import dj_database_url
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
