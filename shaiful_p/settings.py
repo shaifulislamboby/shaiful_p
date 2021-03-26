db5f8d95695f4aaec10e0228c2108ee7b9c4126a
islam <md.shaiful.islam@protonmail.com>
2021-03-26 11:03:02 +0100

    BUG related to static file

--- shaiful_p/settings.py
+++ shaiful_p/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -135,4 +136,4 @@ STATICFILES_DIRS = (
 )
 
 # For sending emails
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
