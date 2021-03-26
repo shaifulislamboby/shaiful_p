d07dd1ab9205b452dac5c0512693f33c8a6c92b0
islam <md.shaiful.islam@protonmail.com>
2021-03-26 14:55:10 +0100

    index changes

--- shaiful_p/settings.py
+++ shaiful_p/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'shaiful_p.wsgi.application'
 import dj_database_url
 DATABASES = {}
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
